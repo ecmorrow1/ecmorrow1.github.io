1a81e2d3656b3e6a86d9d97f9a9bca096364ab12
unknown <edward.c.morrow@gmail.com>
2022-11-15 23:45:26 -0800

    Fixed bug that was misplacing elements between projects when one of the projects had an empty string for a value

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,12 @@
-// import the dictionary from data.js
+// Import the list of dictionaries from data.js
 const projects = data;
+// Grab the wrapper element from index.html
 const parent = document.getElementById("projectList");
 
 var newDiv;
 var anchor;
 var projectDescription;
-var projectlanguages;
+var projectLanguages;
 var projectTools;
 var projectLibraries;
 
@@ -15,7 +16,7 @@ for (var i = 0; i < data.length; i++) {
     // Only populate dictionaries that have a name identified
     if (data[i].name) {
 
-        //Create a new div for each element of data.js
+        // Create a new div for each element of data.js
         newDiv = document.createElement("div");
         newDiv.classList.add("col-md-12");
 
@@ -24,30 +25,36 @@ for (var i = 0; i < data.length; i++) {
         anchor.href = data[i].projectURL;
         anchor.innerHTML = '<h3>'+data[i].name+'</h3>';
 
-        //Create the h6 element to contain the description
+        // Create the h6 element to contain the description
         projectDescription = document.createElement("h6");
         projectDescription.textContent = data[i].description;
-
+        
+        // Create the paragraph element to contain the list of languages utilized
+        projectLanguages = document.createElement("p");
         if (data[i].languages) {
-            //Create the paragraph element to contain the list of languages utilized
-            projectlanguages = document.createElement("p")
-            projectlanguages.textContent = 'Languages Utilized: ' + data[i].languages;
+            projectLanguages.textContent = 'Languages Utilized: ' + data[i].languages;
+        } else {
+            projectLanguages.textContent = "";
         }
 
+        // Create the paragraph element to contain the list of tools utilized
+        projectTools = document.createElement("p");
         if (data[i].tools) {
-            //Create the paragraph element to contain the list of tools utilized
-            projectTools = document.createElement("p")
             projectTools.textContent = 'Tools Utilized: ' + data[i].tools;
+        } else {
+            projectTools.textContent = "";
         }
 
+        // Create the paragraph element to contain the list of libraries utilized
+        projectLibraries = document.createElement("p");
         if (data[i].libraries) {
-            //Create the paragraph element to contain the list of libraries utilized
-            projectLibraries = document.createElement("p")
             projectLibraries.textContent = 'Libraries Utilized: ' + data[i].libraries;
+        } else {
+            projectLibraries.textContent = "";
         }
 
         //Append all new elements to the new div
-        newDiv.append(anchor,projectDescription,projectlanguages,projectTools,projectLibraries);
+        newDiv.append(anchor,projectDescription,projectLanguages,projectTools,projectLibraries);
 
         //Append the new div to the wrapper
         parent.appendChild(newDiv);
