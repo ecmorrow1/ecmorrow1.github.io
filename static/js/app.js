36cf1132fe306b12a27e9895d278fc02a970c92c
unknown <edward.c.morrow@gmail.com>
2022-11-15 23:24:10 -0800

    Updated app.js to only populate languages, tools, or libraries fields if the dictionary contains information for those specific fields

--- static/js/app.js
+++ static/js/app.js
@@ -11,35 +11,46 @@ var projectLibraries;
 
 
 for (var i = 0; i < data.length; i++) {
-    //Create a new div for each element of data.js
-    newDiv = document.createElement("div");
-    newDiv.classList.add("col-md-12");
 
-    // Create the anchor element to be the header
-    anchor = document.createElement("a");
-    anchor.href = data[i].projectURL;
-    anchor.innerHTML = '<h3>'+data[i].name+'</h3>';
+    // Only populate dictionaries that have a name identified
+    if (data[i].name) {
 
-    //Create the paragraph element to contain the description
-    projectDescription = document.createElement("p");
-    projectDescription.textContent = data[i].description;
+        //Create a new div for each element of data.js
+        newDiv = document.createElement("div");
+        newDiv.classList.add("col-md-12");
 
-    //Create the paragraph element to contain the list of languages utilized
-    projectlanguages = document.createElement("p")
-    projectlanguages.textContent = 'Languages Utilized: ' + data[i].languages;
+        // Create the anchor element to be the header
+        anchor = document.createElement("a");
+        anchor.href = data[i].projectURL;
+        anchor.innerHTML = '<h3>'+data[i].name+'</h3>';
 
-    //Create the paragraph element to contain the list of tools utilized
-    projectTools = document.createElement("p")
-    projectTools.textContent = 'Tools Utilized: ' + data[i].tools;
+        //Create the h6 element to contain the description
+        projectDescription = document.createElement("h6");
+        projectDescription.textContent = data[i].description;
 
-    //Create the paragraph element to contain the list of libraries utilized
-    projectLibraries = document.createElement("p")
-    projectLibraries.textContent = 'Libraries Utilized: ' + data[i].libraries;
+        if (data[i].languages) {
+            //Create the paragraph element to contain the list of languages utilized
+            projectlanguages = document.createElement("p")
+            projectlanguages.textContent = 'Languages Utilized: ' + data[i].languages;
+        }
 
-    //Append all new elements to the new div
-    newDiv.append(anchor,projectDescription,projectlanguages,projectTools,projectLibraries);
+        if (data[i].tools) {
+            //Create the paragraph element to contain the list of tools utilized
+            projectTools = document.createElement("p")
+            projectTools.textContent = 'Tools Utilized: ' + data[i].tools;
+        }
 
-    //Append the new div to the wrapper
-    parent.appendChild(newDiv);
+        if (data[i].libraries) {
+            //Create the paragraph element to contain the list of libraries utilized
+            projectLibraries = document.createElement("p")
+            projectLibraries.textContent = 'Libraries Utilized: ' + data[i].libraries;
+        }
+
+        //Append all new elements to the new div
+        newDiv.append(anchor,projectDescription,projectlanguages,projectTools,projectLibraries);
+
+        //Append the new div to the wrapper
+        parent.appendChild(newDiv);
+    }
 }
 
