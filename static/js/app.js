3ba93104ce266cccb5c156fcfe1d78f624a7e027
unknown <edward.c.morrow@gmail.com>
2022-11-22 00:34:07 -0800

    Changed the js to have all generated code contained within the anchor, which dramatically simplified the layout of the code and reduce the number of lines required for executing the main page while simultaneously improving site navigation.

--- static/js/app.js
+++ static/js/app.js
@@ -5,10 +5,6 @@ const parentWrapper = document.getElementById("projectList");
 
 var newDiv;
 var anchor;
-var projectDescription;
-var projectLanguages;
-var projectTools;
-var projectLibraries;
 
 
 for (var i = 0; i < data.length; i++) {
@@ -25,49 +21,36 @@ for (var i = 0; i < data.length; i++) {
         // Create the anchor element to be the header
         anchor = document.createElement("a");
         anchor.href = data[i].projectURL;
-        anchor.innerHTML = '<h3>'+data[i].name+'</h3>';
 
-        // Create the h6 element to contain the description
-        projectDescription = document.createElement("h6");
-        projectDescription.innerHTML = data[i].description;
+        // Create an h3 and h6 element to hold the project name and the description
+        anchor.innerHTML = '<h3>' + data[i].name + '</h3><h6>' + data[i].description + '</h6>';
         
-        // Create the paragraph element to contain the list of languages utilized
-        projectLanguages = document.createElement("p");
+        // Add the list of languages to the anchor if there are any
         if (data[i].languages[0]) {
-            projectLanguages.innerHTML = 'Languages Utilized: <b>' + data[i].languages[0] + '</b>';
+            anchor.innerHTML = anchor.innerHTML + 'Languages Utilized: <b>' + data[i].languages[0] + '</b>';
             for (var j=1;j<data[i].languages.length;j++) {
-                projectLanguages.innerHTML = projectLanguages.innerHTML + ' | <b>' + data[i].languages[j] + '</b>';
+                anchor.innerHTML = anchor.innerHTML + ' | <b>' + data[i].languages[j] + '</b>';
             }
-        } else {
-            projectLanguages.innerHTML = '';
         }
-
-
-        // Create the paragraph element to contain the list of tools utilized
-        projectTools = document.createElement("p");
+        
+        // Add the list of tools to the anchor if there are any
         if (data[i].tools[0]) {
-            projectTools.innerHTML = 'Tools Utilized: <b>' + data[i].tools[0] + '</b>';
-            for (var j=1;j<data[i].tools.length;j++) {
-                projectTools.innerHTML = projectTools.innerHTML + ' | <b>' + data[i].tools[j] + '</b>';
+            anchor.innerHTML = anchor.innerHTML + '<br><br>Tools Utilized: <b>' + data[i].tools[0] + '</b>';
+            for (var j=1;j<data[i].tools;j++) {
+                anchor.innerHTML = anchor.innerHTML + ' | <b>' + data[i].tools[j] + '</b>';
             }
-        } else {
-            projectTools.innerHTML = '';
         }
-
-
-        // Create the paragraph element to contain the list of libraries utilized
-        projectLibraries = document.createElement("p");
+        
+        // Add the list of libraries to the anchor if there are any
         if (data[i].libraries[0]) {
-            projectLibraries.innerHTML = 'Libraries Utilized: <b>' + data[i].libraries[0] + '</b>';
+            anchor.innerHTML = anchor.innerHTML + '<br><br>Libraries Utilized: <b>' + data[i].libraries[0] + '</b>';
             for (var j=1;j<data[i].libraries.length;j++) {
-                projectLibraries.innerHTML = projectLibraries.innerHTML + ' | <b>' + data[i].libraries[j] + '</b>';
+                anchor.innerHTML = anchor.innerHTML + ' | <b>' + data[i].libraries[j] + '</b>';
             }
-        } else {
-            projectLibraries.innerHTML = '';
         }
 
-        //Append all new elements to the new div
-        newDiv.append(anchor,projectDescription,projectLanguages,projectTools,projectLibraries);
+        // append the anchor to the newDiv
+        newDiv.append(anchor);
 
         //Append the new div to the wrapper
         parentWrapper.appendChild(newDiv);
